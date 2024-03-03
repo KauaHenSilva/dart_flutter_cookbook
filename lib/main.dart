5811a95147db3bffad11b3a13ba3c7b7773be379
Kaua Hen <kaua.da@ufpi.edu.br>
2024-03-03 14:25:34 -0300

    Init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:dart_flutter_cookbooks/routes/app_routes.dart';
+import 'package:dart_flutter_cookbooks/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cookbook App',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      routes: {
+        AppRoutes.home: (context) => const HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,5 @@
+
+
+class AppRoutes {
+  static const String home = '/';
+}
